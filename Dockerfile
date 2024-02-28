50502ab21b05ab28c1d9d5ec7908b9662ebcd72e
Tim Zubov <timofey.zubov.05@gmail.com>
2024-02-28 23:53:18 +0300

    Add dockerfile for building my web-app image and prometheus

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.10
+
+WORKDIR /flask-app
+
+COPY requirements.txt .
+
+RUN pip3 install -r requirements.txt --no-cache-dir
+
+COPY . .
+
+CMD ["gunicorn", "-b", "0.0.0.0:8000", "my_app:app"]
--- my_app.py
+++ my_app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask import render_template
+from prometheus_flask_exporter import PrometheusMetrics
+from dotenv import load_dotenv
+
+
+load_dotenv('.flask-prometh-env')
 
 app = Flask(__name__, template_folder='templates')
+metrics = PrometheusMetrics(app)
 
 
+# app.config['SERVER_NAME'] = 'flask-app:8000'
 @app.route('/')
 def hello_world():
     return 'Hello World!'
